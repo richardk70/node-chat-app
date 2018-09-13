e2a3990f734d1ca934e841119a07a9f468a6cd02
Richard Keightley <richardk70@gmail.com>
2018-09-13 16:19:06 -0700

    =Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 // routes
 // app.listen
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public/');
 
@@ -9,15 +11,35 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-// routes
-// app.get('/', (req, res) => {
-//     res.render('index.html');
-// })
+io.on('connection', (socket) => {
+    console.log('New user connected.');
 
-app.listen(port, (err, res) => {
-    if (err) throw err;
+    socket.on('disconnect', (socket) => {
+        console.log('User disconnected.');
+    });
 
+    socket.emit('newMessage', {
+        from: 'messenger@home.com',
+        text: 'this is the text messsage',
+        createdAt: new Date()
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        var hours = new Date().getHours()%12;
+        var minutes = new Date().getMinutes();
+        var secs = new Date().getSeconds();
+        var createdAt = `${hours}:${minutes}:${secs}`;
+        console.log(newMessage);
+        console.log('created at: ', createdAt);
+    });
+
+});
+
+server.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
